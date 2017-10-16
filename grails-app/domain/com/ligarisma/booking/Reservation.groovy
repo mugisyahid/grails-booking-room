85a7a469e70dceb122ef7779035421a9c54a65eb
Ligar Mugi Syahid <mugisyahid@gmail.com>
2017-10-16 11:47:52 +0700

    init commit with 1 domain

---
+++ grails-app/domain/com/ligarisma/booking/Reservation.groovy
@@ -0,0 +1,7 @@
+package com.ligarisma.booking
+
+class Reservation {
+
+    static constraints = {
+    }
+}
---
+++ grails-app/init/grails/booking/room/BootStrap.groovy
@@ -0,0 +1,9 @@
+package grails.booking.room
+
+class BootStrap {
+
+    def init = { servletContext ->
+    }
+    def destroy = {
+    }
+}
